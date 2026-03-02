1f08d276e1497fa840017cfb64d15a6b1ae244d9
Flambe02 <thepimentaorougecompany@gmail.com>
2026-03-02 20:16:42 -0300

    Render GTM snippet as raw head script for detector compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 ﻿import type { Metadata } from 'next';
 import { Inter, Poppins } from 'next/font/google';
-import Script from 'next/script';
 import { ClarityInit } from '@/components/ClarityInit';
 import './globals.css';
 import './discutai-widget-fix.css';
@@ -106,15 +105,15 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
         {gtmContainerId ? (
-          <Script id="google-tag-manager" strategy="beforeInteractive">
-            {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmContainerId}');
-            `}
-          </Script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmContainerId}');`,
+            }}
+          />
         ) : null}
       </head>
       <body className={`${inter.className} ${poppins.variable} antialiased`} suppressHydrationWarning>
