1a311e2da5bb040ad80ff97b5fc6a83d67e72b9d
Flambe02 <thepimentaorougecompany@gmail.com>
2026-03-03 16:33:19 -0300

    Remove automatic translation script from demo pages

--- components/GTranslateLoader.tsx
+++ components/GTranslateLoader.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const GTranslateWidget = dynamic(() => import('@/components/GTranslateWidget'), {
-  ssr: false,
-});
-
 export default function GTranslateLoader() {
-  return <GTranslateWidget />;
+  return null;
 }
